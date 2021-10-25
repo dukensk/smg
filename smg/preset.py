c6925ccc7f80ef14cbd5a518f774424627688dfc
dukensk <duke.nsk@gmail.com>
2021-10-26 06:26:47 +0700

    upd: remove trash

--- smg/preset.py
+++ smg/preset.py
@@ -54,7 +54,6 @@ output_formats = [
     OutputFormat(label='Видео, 1080p, webm', preset='bestvideo[height<=1080]+bestaudio/best[height<=1080]'),
     OutputFormat(label='Видео, 1440p, webm', preset='bestvideo[height<=1440]+bestaudio/best[height<=1440]'),
     OutputFormat(label='Видео, 4K, webm', preset='bestvideo[height<=4096]+bestaudio/best[height<=4096]'),
-    # OutputFormat(label='Видео, 720p', preset='bestvideo[height<=720]+bestaudio/best[height<=720]'),
     OutputFormat(label='Видео в максимальном качестве', preset='bestvideo+bestaudio/best'),
     OutputFormat(label='Аудио в максимальном качестве (оптимально для Twitch)', preset='bestaudio/best',
                  postprocessors=[{'key': 'FFmpegExtractAudio', 'preferredcodec': 'm4a', 'preferredquality': '192'}]),
