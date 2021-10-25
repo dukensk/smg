8d40b4493a181a96aae0c0451db5d50ae8db8d30
dukensk <duke.nsk@gmail.com>
2021-10-25 22:33:40 +0700

    add: support for settings via environment variables

--- settings.py
+++ settings.py
@@ -1,6 +1,14 @@
-from smg.preset import OutputFormat
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 
-SAVE_PATH = 'd:/Sort/Downloads/YouTube'
+from smg.preset import OutputFormat
+from smg.service import get_system_downloads_path
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+SAVE_PATH = os.environ.get('SAVE_PATH', default=get_system_downloads_path())
 '''Путь для сохранения скачанных файлов'''
 
 output_formats = [
