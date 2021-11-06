de494b46a0903003cc90f3cc63dc99ad80aff514
dukensk <duke.nsk@gmail.com>
2021-11-07 06:04:16 +0700

    fix: removing temporary files

--- translator/service.py
+++ translator/service.py
@@ -168,7 +168,7 @@ def merge_video_and_vo(original_video_path: str, vo_audio_path: str):
     output_video_path = replace_audio_in_video(original_video_path, merged_audio_path)
 
     print('Переведенный файл: ' + output_video_path)
-    # remove_temp_files(temp_files)
+    remove_temp_files(temp_files)
 
 
 def merge_audio_and_vo(original_audio_path: str, vo_audio_path: str):
