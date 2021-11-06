3365d320c4d5b3603cd0be912ae1e0038dd85c4f
dukensk <duke.nsk@gmail.com>
2021-11-06 13:57:03 +0700

    add: output file preset types

--- smg/preset.py
+++ smg/preset.py
@@ -7,9 +7,15 @@ class OutputFormat:
     label: str = None
     preset: str = None
     postprocessors: list[dict[str, str]] = None
+    type: str = None
 
     def __str__(self):
         return self.label
+
+    @property
+    def is_audio_only(self):
+        return True if self.type == 'audio' else False
+
 
 
 class OutputFormatMenu:
@@ -55,7 +61,7 @@ class OutputFormatMenu:
 
 output_formats = [
     OutputFormat(label='Аудио, m4a  (оптимально для YouTube)', preset='bestaudio[ext=m4a]/best',
-                 postprocessors=[{'key': 'FFmpegExtractAudio', 'preferredcodec': 'm4a', 'preferredquality': '128'}]),
+                 postprocessors=[{'key': 'FFmpegExtractAudio', 'preferredcodec': 'm4a', 'preferredquality': '128'}], type='audio'),
     OutputFormat(label='Видео, 1080p, mp4', preset='bestvideo[height<=1080][ext=mp4]+bestaudio[ext=m4a]/best[height<=1080]'),
     OutputFormat(label='Видео, 1440, mp4', preset='bestvideo[height<=1440][ext=mp4]+bestaudio[ext=m4a]/best[height<=1440]'),
     OutputFormat(label='Видео, 4K, mp4', preset='bestvideo[height<=4096][ext=mp4]+bestaudio[ext=m4a]/best[height<=4096]'),
@@ -64,5 +70,5 @@ output_formats = [
     OutputFormat(label='Видео, 4K, webm', preset='bestvideo[height<=4096]+bestaudio/best[height<=4096]'),
     OutputFormat(label='Видео в максимальном качестве', preset='bestvideo+bestaudio/best'),
     OutputFormat(label='Аудио в максимальном качестве (оптимально для Twitch)', preset='bestaudio/best',
-                 postprocessors=[{'key': 'FFmpegExtractAudio', 'preferredcodec': 'm4a', 'preferredquality': '192'}]),
+                 postprocessors=[{'key': 'FFmpegExtractAudio', 'preferredcodec': 'm4a', 'preferredquality': '192'}], type='audio'),
 ]
--- translator/preset.py
+++ translator/preset.py
@@ -2,10 +2,10 @@ from smg.preset import OutputFormat
 
 output_formats = [
     OutputFormat(label='Аудио, m4a  (оптимально для YouTube)', preset='bestaudio[ext=m4a]/best',
-                 postprocessors=[{'key': 'FFmpegExtractAudio', 'preferredcodec': 'm4a', 'preferredquality': '128'}]),
+                 postprocessors=[{'key': 'FFmpegExtractAudio', 'preferredcodec': 'm4a', 'preferredquality': '128'}], type='audio'),
     OutputFormat(label='Видео, 1080p, mp4', preset='bestvideo[height<=1080][ext=mp4]+bestaudio[ext=m4a]/best[height<=1080]'),
     OutputFormat(label='Видео, 1440, mp4', preset='bestvideo[height<=1440][ext=mp4]+bestaudio[ext=m4a]/best[height<=1440]'),
     OutputFormat(label='Видео, 4K, mp4', preset='bestvideo[height<=4096][ext=mp4]+bestaudio[ext=m4a]/best[height<=4096]'),
     OutputFormat(label='Аудио в максимальном качестве (оптимально для Twitch)', preset='bestaudio/best',
-                 postprocessors=[{'key': 'FFmpegExtractAudio', 'preferredcodec': 'm4a', 'preferredquality': '192'}]),
+                 postprocessors=[{'key': 'FFmpegExtractAudio', 'preferredcodec': 'm4a', 'preferredquality': '192'}], type='audio'),
 ]
