504615d7249f424056a122c68ce066c353e4b483
dukensk <duke.nsk@gmail.com>
2021-11-08 09:51:18 +0700

    add: обработка ошибки скачивания закадрового перевода

--- translator.py
+++ translator.py
@@ -1,3 +1,5 @@
+import urllib
+
 import colorama
 import wget
 import yt_dlp
@@ -21,9 +23,18 @@ def main():
         output_format = choose_output_format(output_format_menu)
 
         vo_url = input('Введите URL закадрового перевода: ')
-        print('\nСкачиваем закадровый перевод')
-        vo_path = wget.download(vo_url, out=settings.TRANSLATOR_TEMP_PATH, bar=wget.bar_adaptive)
-        print(Style.DIM + Fore.GREEN + '\nЗАКАДРОВЫЙ ПЕРЕВОД СКАЧАН' + Style.RESET_ALL)
+
+        download_attempt = 0
+        vo_path = None
+        while not vo_path and download_attempt < settings.TRANSLATOR_DOWNLOAD_ATTEMPTS_LIMIT:
+            try:
+                print('\nСкачиваем закадровый перевод')
+                vo_path = wget.download(vo_url, out=settings.TRANSLATOR_TEMP_PATH, bar=wget.bar_adaptive)
+                print(Style.DIM + Fore.GREEN + '\nЗАКАДРОВЫЙ ПЕРЕВОД СКАЧАН' + Style.RESET_ALL)
+            except urllib.error.ContentTooShortError:
+                print(Style.DIM + Fore.RED + '\nНЕ УДАЛОСЬ СКАЧАТЬ ЗАКАДРОВЫЙ ПЕРЕВОД, ПРОБУЕМ СНОВА' + Style.RESET_ALL)
+                download_attempt += 1
+                vo_path = None
 
         print('\nСкачиваем видео:')
         ydl_opts = {
@@ -37,19 +48,25 @@ def main():
             ydl_opts['postprocessors'] = output_format.postprocessors
 
         try:
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                yinfo = ydl.extract_info(video_url, download=True)
-            print(Style.DIM + Fore.GREEN + 'ВИДЕО СКАЧАНО' + Style.RESET_ALL)
+            if vo_url:
+                with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                    yinfo = ydl.extract_info(video_url, download=True)
+                print(Style.DIM + Fore.GREEN + 'ВИДЕО СКАЧАНО' + Style.RESET_ALL)
 
-            original_file_path = ydl.prepare_filename(yinfo)
+                original_file_path = ydl.prepare_filename(yinfo)
 
-            if output_format.is_audio_only:
-                merge_audio_and_vo(original_file_path, vo_path)
+                if output_format.is_audio_only:
+                    merge_audio_and_vo(original_file_path, vo_path)
+                else:
+                    merge_video_and_vo(original_file_path, vo_path)
+
+                print(Style.DIM + Fore.GREEN + '\nГОТОВО' + Style.RESET_ALL)
+                print('Все операции успешно завершены. Можно скачать что-нибудь еще.')
             else:
-                merge_video_and_vo(original_file_path, vo_path)
+                print(Style.DIM + Fore.RED + '\n\nYOU DIED' + Style.RESET_ALL)
+                print('\nБез закадрового перевода нет смысла качать видео. :('
+                      '\nПопробуйте скачать что-то еще.')
 
-            print(Style.DIM + Fore.GREEN + '\nГОТОВО' + Style.RESET_ALL)
-            print('Все операции успешно завершены. Можно скачать что-нибудь еще.')
         except yt_dlp.DownloadError:
             print(Style.DIM + Fore.RED + '\n\nYOU DIED' + Style.RESET_ALL)
             print('\nНе удалось скачать видео. Нам очень жаль. :('
