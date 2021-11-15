5775b5341ec0513cdc459cc821cb8f9b42ad0c3d
dukensk <duke.nsk@gmail.com>
2021-11-15 16:04:12 +0700

    add: function to get the system default downloads path

---
+++ smg/filesystem.py
@@ -0,0 +1,15 @@
+import os
+from pathlib import Path
+
+
+def get_system_downloads_path() -> Path:
+    """Returns the default downloads path for linux or windows"""
+    if os.name == 'nt':
+        import winreg
+        sub_key = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders'
+        downloads_guid = '{374DE290-123F-4565-9164-39C4925E467B}'
+        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, sub_key) as key:
+            location = winreg.QueryValueEx(key, downloads_guid)[0]
+        return Path(location)
+    else:
+        return Path.home() / 'Downloads'
