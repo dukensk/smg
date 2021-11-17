78c99021d48c52110e4955955971a71151fb6af5
dukensk <duke.nsk@gmail.com>
2021-11-18 00:54:01 +0700

    add: getting the duration of a media file

--- smg/media.py
+++ smg/media.py
@@ -1,11 +1,26 @@
+import subprocess
+
 from smg.filesystem import File
 
 
-class AudioFile(File):
+class MediaFile(File):
+
+    @property
+    def duration(self) -> float:
+        """Duration in seconds"""
+        result = subprocess.run(["ffprobe", "-v", "error", "-show_entries",
+                                 "format=duration", "-of",
+                                 "default=noprint_wrappers=1:nokey=1", self.path],
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.STDOUT)
+        return float(result.stdout)
+
+
+class AudioFile(MediaFile):
     EXTENSION_MP3 = '.mp3'
     EXTENSION_M4A = '.m4a'
 
 
-class VideoFile(File):
+class VideoFile(MediaFile):
     EXTENSION_MP4 = '.mp4'
     EXTENSION_MKV = '.mkv'
