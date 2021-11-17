ca576627ba5d7e4f2429b44e5bbf0fec3888c425
dukensk <duke.nsk@gmail.com>
2021-11-18 02:05:56 +0700

    add: getting mediainfo

--- smg/media.py
+++ smg/media.py
@@ -1,19 +1,41 @@
 import subprocess
+from json import loads
+from pathlib import Path
+from subprocess import check_output
 
 from smg.filesystem import File
 
 
 class MediaFile(File):
+    _media_info: dict
+
+    def __init__(self, path: Path):
+        super(MediaFile, self).__init__(path)
+        self._media_info = {}
+
+    def _clear_media_info(self):
+        self._media_info.clear()
+
+    def remove(self):
+        super(MediaFile, self).remove()
+        self._clear_media_info()
 
     @property
     def duration(self) -> float:
         """Duration in seconds"""
-        result = subprocess.run(["ffprobe", "-v", "error", "-show_entries",
-                                 "format=duration", "-of",
-                                 "default=noprint_wrappers=1:nokey=1", self.path],
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.STDOUT)
-        return float(result.stdout)
+        return float(self.media_info.get('format').get('duration'))
+
+    @property
+    def media_info(self) -> dict:
+        if not self._media_info:
+            result = check_output(['ffprobe',
+                                   '-hide_banner', '-loglevel', 'panic',
+                                   '-show_format',
+                                   '-show_streams',
+                                   '-of',
+                                   'json', self.path])
+            self._media_info = loads(result)
+        return self._media_info
 
 
 class AudioFile(MediaFile):
