d8668128e670666952fa7026bae93b3468143912
dukensk <duke.nsk@gmail.com>
2021-11-22 16:29:41 +0700

    add: functions to safely retrieve list items and get controller_key from parameters passed to the script

---
+++ smg/service.py
@@ -0,0 +1,14 @@
+import sys
+
+
+def safe_list_get(lst: list, index: int, default=None):
+    """Safely retrieves an element of the list by index, if the element is absent, returns the default value"""
+    try:
+        return lst[index]
+    except IndexError:
+        return default
+
+
+def get_controller_key() -> str | None:
+    """Gets the controller_key from parameters passed to the script at startup"""
+    return safe_list_get(sys.argv, 1)
