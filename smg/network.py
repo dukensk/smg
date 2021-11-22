6204011f7e72c25a8ee3e0691209b84963fc4ae7
dukensk <duke.nsk@gmail.com>
2021-11-22 19:48:25 +0700

    upd: move FileDownloader to smg.downloaders

--- smg/network.py
+++ smg/network.py
@@ -1,12 +1,4 @@
-import urllib
-from pathlib import Path
-from urllib.error import ContentTooShortError
-from colorama import Fore, Style
 import validators
-import wget
-
-import settings
-from smg.filesystem import File
 
 
 def is_url(url: str) -> bool:
@@ -14,63 +6,3 @@ def is_url(url: str) -> bool:
     return False if not url or not validators.url(url) else True
 
 
-class FileDownloader:
-    """file downloader"""
-
-    INPUT_URL_MESSAGE = 'URL скачиваемого файла'
-    """message when requesting file url"""
-
-    INPUT_URL_ERROR_MESSAGE = 'ЭТО НЕКОРРЕКТНЫЙ URL, ПОПРОБУЙТЕ ВВЕСТИ ДРУГОЙ'
-    START_DOWNLOADING_MESSAGE = 'Скачиваем файл...'
-    FINISH_DOWNLOADING_MESSAGE = 'ФАЙЛ УСПЕШНО СКАЧАН'
-    ERROR_DOWNLOADING_MESSAGE = 'НЕ УДАЛОСЬ СКАЧАТЬ ФАЙЛ, ПРОБУЕМ СНОВА'
-
-    _url: str
-
-    def __init__(self, url: str = None):
-        self._init_url(url)
-
-    def _init_url(self, url: str | None = None):
-        if not is_url(url):
-            url = self._input_url()
-        self._url = url
-
-    def _input_url(self) -> str:
-        url = None
-        while not is_url(url):
-            url = input('\n' + self.INPUT_URL_MESSAGE + ': ')
-            if not is_url(url):
-                print(Style.DIM + Fore.RED + self.INPUT_URL_ERROR_MESSAGE + Style.RESET_ALL)
-        return url
-
-    @property
-    def _save_path(self) -> Path:
-        return settings.SAVE_PATH
-
-    def _show_start_downloading_message(self):
-        print('\n' + self.START_DOWNLOADING_MESSAGE)
-
-    def _show_finish_downloading_message(self):
-        print(Style.DIM + Fore.GREEN + '\n' + self.FINISH_DOWNLOADING_MESSAGE + Style.RESET_ALL)
-
-    def _show_error_downloading_message(self):
-        print(Style.DIM + Fore.RED + '\n' + self.ERROR_DOWNLOADING_MESSAGE + Style.RESET_ALL)
-
-    def _create_file(self, path: Path) -> File:
-        return File(path)
-
-    def download(self) -> File | None:
-        """Downloads a file"""
-        download_attempt = 0
-        file_path = None
-        while not file_path and download_attempt < settings.DOWNLOAD_ATTEMPTS_LIMIT:
-            try:
-                self._show_start_downloading_message()
-                file_path = wget.download(self._url, out=str(self._save_path), bar=wget.bar_adaptive)
-                self._show_finish_downloading_message()
-            except urllib.error.ContentTooShortError:
-                self._show_error_downloading_message()
-                download_attempt += 1
-                file_path = None
-
-        return self._create_file(Path(file_path)) if file_path else None
