f0c6687c2924d6ba607dff0cf6d51e91cf35df6e
dukensk <duke.nsk@gmail.com>
2021-11-23 07:06:32 +0700

    upd: formatting

--- smg/service.py
+++ smg/service.py
@@ -6,5 +6,3 @@ from common.service import safe_list_get
 def get_controller_key() -> str | None:
     """Gets the controller_key from parameters passed to the script at startup"""
     return safe_list_get(sys.argv, 1)
-
-
