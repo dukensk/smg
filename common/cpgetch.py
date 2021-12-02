46ea72f57dfc07e015198df4f6970b65a4e535a8
dukensk <duke.nsk@gmail.com>
2021-12-03 04:36:21 +0700

    add: key_codes and get_key_code

--- common/cpgetch.py
+++ common/cpgetch.py
@@ -46,4 +46,25 @@ class _GetchWindows:
         return msvcrt.getch()
 
 
-getch = _Getch()
+key_codes = {
+    'enter': b'\r',
+    'esc': b'\x1b',
+    'backspace': b'\x08',
+    'tab': b'\t',
+    'space': b' ',
+    '1': b'1',
+    '2': b'2',
+    '3': b'3',
+    '4': b'4',
+    '5': b'5',
+    '6': b'6',
+    '7': b'7',
+    '8': b'8',
+    '9': b'9',
+    '0': b'0',
+}
+
+
+def get_key_code(key: str) -> bytes | None:
+    """Returns the code corresponding to the keys on the keyboard"""
+    return key_codes.get(key)
