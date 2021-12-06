260933cb2210c0f8423b07616f08e4cd8aaa9aa2
dukensk <duke.nsk@gmail.com>
2021-12-06 16:05:09 +0700

    add: media_downloaders menu to translator package

---
+++ translator/menu.py
@@ -0,0 +1,9 @@
+from common.menu import MenuItem
+from mediagrabber.downloaders import M4aAudioDownloader, Mp4x1080pVideoDownloader, Mp4x1440pVideoDownloader, Mp4x4KVideoDownloader
+
+media_downloaders = {
+    'audio_m4a': MenuItem(M4aAudioDownloader.title, M4aAudioDownloader),
+    'video_mp4_1080p': MenuItem(Mp4x1080pVideoDownloader.title, Mp4x1080pVideoDownloader),
+    'video_mp4_1440p': MenuItem(Mp4x1440pVideoDownloader.title, Mp4x1440pVideoDownloader),
+    'video_mp4_4k': MenuItem(Mp4x4KVideoDownloader.title, Mp4x4KVideoDownloader),
+}
