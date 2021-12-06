1960c10e12f0c1666e2a3837e76606390c96c3dd
dukensk <duke.nsk@gmail.com>
2021-12-06 19:31:34 +0700

    add: функция для проверки, принадлежит ли домен YouTube

--- common/service.py
+++ common/service.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from urllib.parse import urlparse
 
 import validators
 
@@ -20,3 +21,26 @@ def is_url(url: str) -> bool:
 def init_working_directory(path: Path):
     """Initializing the application working directory"""
     os.chdir(path)
+
+
+def is_youtube(url: str) -> bool:
+    """Checks if the domain belongs to YouTube"""
+    if not is_url(url):
+        return False
+
+    youtube_domains = [
+        'www.youtube.com',
+        'youtube.com',
+        'www.youtu.be',
+        'youtu.be',
+        'youtube.com.br',
+        'youtube.co.nz',
+        'youtube.de',
+        'youtube.es',
+        'youtube.it',
+        'youtube.nl',
+        'youtube-nocookie.com',
+        'youtube.ru',
+    ]
+    domain = urlparse(url).netloc
+    return domain in youtube_domains
