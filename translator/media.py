101db913ec58a7030147b410645c817e2802efb0
dukensk <duke.nsk@gmail.com>
2021-12-06 16:37:50 +0700

    add: TranslatableMediaFile, TranslatableAudioFile, TranslatableVideoFile, convert_to_translatable_mediafile

--- translator/media.py
+++ translator/media.py
@@ -1,10 +1,11 @@
 import subprocess
 from pathlib import Path
+from abc import ABC, abstractmethod
 
 from colorama import Style, Fore
 
 import settings
-from common.media import AudioFile
+from common.media import AudioFile, VideoFile
 
 
 class VoiceOver(AudioFile):
@@ -41,6 +42,50 @@ class VoiceOver(AudioFile):
                             stdout=subprocess.DEVNULL,
                             stderr=subprocess.STDOUT)
             self._replace_path_and_remove_old_file(self._preprocessed_path)
+            self._preprocessed = True
             print(Style.DIM + Fore.GREEN + 'ГОТОВО' + Style.RESET_ALL)
         else:
             print('\nЗакадровый перевод уже подготовлен, дополнительная обработка не требуется')
+
+
+class TranslatableMediaFile(ABC):
+    """Media file to which you can add voiceover"""
+
+    @abstractmethod
+    def add_voiceover(self, voiceover: VoiceOver) -> bool:
+        """Adds voice-over translation"""
+
+
+class TranslatableAudioFile(AudioFile, TranslatableMediaFile):
+    """Audio file to which you can add voiceover"""
+
+    def add_voiceover(self, voiceover: VoiceOver) -> bool:
+        pass
+
+
+class TranslatableVideoFile(VideoFile, TranslatableMediaFile):
+    """Video file to which you can add voiceover"""
+
+    def add_voiceover(self, voiceover: VoiceOver) -> bool:
+        pass
+
+
+def convert_to_translatable_mediafile(mediafile: VideoFile | AudioFile) -> TranslatableAudioFile | TranslatableVideoFile | None:
+    """Converts a media object to translatable"""
+    file_types = {
+        # audio
+        TranslatableAudioFile.EXTENSION_MP3: TranslatableAudioFile,
+        TranslatableAudioFile.EXTENSION_AAC: TranslatableAudioFile,
+        TranslatableAudioFile.EXTENSION_M4A: TranslatableAudioFile,
+        TranslatableAudioFile.EXTENSION_OGG: TranslatableAudioFile,
+        TranslatableAudioFile.EXTENSION_WAV: TranslatableAudioFile,
+        # video
+        TranslatableVideoFile.EXTENSION_MP4: TranslatableVideoFile,
+        TranslatableVideoFile.EXTENSION_MKV: TranslatableVideoFile,
+        TranslatableVideoFile.EXTENSION_AVI: TranslatableVideoFile,
+        TranslatableVideoFile.EXTENSION_FLV: TranslatableVideoFile,
+        TranslatableVideoFile.EXTENSION_WEBM: TranslatableVideoFile,
+    }
+
+    file_type = file_types.get(mediafile.extension)
+    return file_type(mediafile.path) if file_type else None
