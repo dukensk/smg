864d97cfe316e8770ed09aa97685669df8b7d37a
dukensk <duke.nsk@gmail.com>
2021-12-06 17:10:57 +0700

    upd: translate of media file to TranslatorController

--- translator/controllers.py
+++ translator/controllers.py
@@ -1,5 +1,8 @@
+from colorama import Style, Fore
+
 from common.controllers import Controller
-from translator.downloaders import VoiceOverDownloader
+from translator.downloaders import VoiceOverDownloader, TranslatableMediaDownloader
+from translator.media import VoiceOver, TranslatableVideoFile, TranslatableAudioFile
 
 
 class TranslatorController(Controller):
@@ -13,6 +16,37 @@ class TranslatorController(Controller):
         return 'Translator'
 
     def main(self) -> bool:
-        downloader = VoiceOverDownloader()
-        # voiceover = downloader.download()
-        return True
+        media_downloader, voiceover_downloader = self._get_downloaders()
+        media_file, voiceover = self._download_media_files(media_downloader, voiceover_downloader)
+        if media_file:
+            media_file.add_voiceover(voiceover)
+            media_file.move_to_downloads()
+            print(f'Скачан медиафайл: {media_file.info}')
+            return True
+        else:
+            return False
+
+    def _get_downloaders(self) -> tuple[TranslatableMediaDownloader, VoiceOverDownloader]:
+        media_downloader = TranslatableMediaDownloader()
+        self.show_app_header()
+        print('\nПолучаем метаданные...')
+        info = media_downloader.info
+        self.show_app_header()
+        print(f'\n{info}\n')
+        voiceover_downloader = VoiceOverDownloader()
+        self.show_app_header()
+        print(f'\n{info}\n')
+        return media_downloader, voiceover_downloader
+
+    def _download_media_files(self, media_downloader: TranslatableMediaDownloader, voiceover_downloader: VoiceOverDownloader) \
+            -> tuple[TranslatableAudioFile | TranslatableVideoFile, VoiceOver]:
+        voiceover = voiceover_downloader.download()
+        if voiceover:
+            media_file = media_downloader.download()
+        else:
+            media_file = None
+            print(f'{Style.DIM}{Fore.RED}  \n\nYOU DIED{Style.RESET_ALL}')
+            print('\nБез закадрового перевода нет смысла качать видео. :('
+                  '\nПопробуйте скачать что-то еще.')
+            print(f'Проблемное  видео: {media_downloader.url}')
+        return media_file, voiceover
