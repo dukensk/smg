ac4a1886735dddfa9839a537c755a66c67d8cac0
dukensk <duke.nsk@gmail.com>
2022-03-14 03:23:42 +0700

    upd: translated comments in settings file

--- settings.py
+++ settings.py
@@ -8,13 +8,13 @@ dotenv_path = Path(__file__).resolve().parent / '.env'
 load_dotenv(str(dotenv_path))
 
 SAVE_PATH = Path(os.environ.get('SAVE_PATH', default=get_system_downloads_path()))
-'''Путь для сохранения скачанных файлов'''
+'''Path to save downloaded files'''
 
 TEMP_PATH = Path(os.environ.get('TEMP_PATH', default=SAVE_PATH / 'temp'))
-'''Путь для временных файлов'''
+'''Path for temporary files'''
 
 TRANSLATOR_SAVE_PATH = os.environ.get('TRANSLATOR_SAVE_PATH', default=SAVE_PATH)
-'''Путь для переведенных файлов'''
+'''Path for translated files'''
 
 TRANSLATOR_VOLUME_ORIGINAL_AUDIO = 0.45
 TRANSLATOR_OUTPUT_AUDIO_BITRATE = '192k'
