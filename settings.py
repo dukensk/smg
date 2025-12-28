89eef54b11b3fa0c08f3ed701f3a72ba92903369
duke <duke.nsk@gmail.com>
2025-12-28 09:45:03 +0700

    fix: formatting

--- settings.py
+++ settings.py
@@ -6,19 +6,19 @@ from dotenv import load_dotenv
 from common.filesystem import get_system_downloads_path
 
 ROOT_PATH = Path(__file__).resolve().parent
-"""Path to the root of the application"""  # noqa: Q001
+'''Path to the root of the application'''
 
 dotenv_path = ROOT_PATH / '.env'
 load_dotenv(str(dotenv_path))
 
 SAVE_PATH = Path(os.environ.get('SAVE_PATH', default=get_system_downloads_path()))
-"""Path to save downloaded files"""  # noqa: Q001
+'''Path to save downloaded files'''
 
 TEMP_PATH = Path(os.environ.get('TEMP_PATH', default=SAVE_PATH / 'temp'))
-"""Path for temporary files"""  # noqa: Q001
+'''Path for temporary files'''
 
 TRANSLATOR_SAVE_PATH = os.environ.get('TRANSLATOR_SAVE_PATH', default=SAVE_PATH)
-"""Path for translated files"""  # noqa: Q001
+'''Path for translated files'''
 
 TRANSLATOR_VOLUME_ORIGINAL_AUDIO = 0.45
 
@@ -31,7 +31,7 @@ DOWNLOAD_ATTEMPTS_LIMIT = 30
 MEDIAGRABBER_DOWNLOAD_ATTEMPTS_LIMIT = 9000
 
 ENABLE_PUSH_NOTIFICATION = os.environ.get('ENABLE_PUSH_NOTIFICATION', default=True)
-"""Allows system push notifications"""  # noqa: Q001
+'''Allows system push notifications'''
 
 ICONS_PATH = ROOT_PATH / 'icons'
 
